e004b99315cda6ec9c393fa14b765eca8440c16d
Tomas Balaz <balaztomas0@gmail.com>
2024-04-11 11:16:34 +0200

    Created ApplicationUser class

---
+++ src/main/java/sk/balaz/springbootsecurity/auth/ApplicationUser.java
@@ -0,0 +1,61 @@
+package sk.balaz.springbootsecurity.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ApplicationUser implements UserDetails {
+
+    private final List<? extends GrantedAuthority> grantedAuthorities;
+
+    private final String password;
+
+    private final String userName;
+
+    private final boolean isAccountNonExpired;
+
+    private final boolean isAccountNonLocked;
+
+    private final boolean isCredentialsNonExpired;
+
+    private final boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
